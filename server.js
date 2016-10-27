532e8e9bb80d2a1f576581427a48ef8fc06ca351
ahza64 <ahza64@yahoo.com>
2016-10-27 00:27:32 -0700

    serving file

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var express = require('express'),
     app = express();
 
 app.get('/', function (req, res) {
-  res.send("hello");
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('server started on port 3000');
 });
