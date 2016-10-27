32d431d7cbe6ea20c8b0e565727a5b7953b19162
ahza64 <ahza64@yahoo.com>
2016-10-27 00:18:00 -0700

    server response

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 var express = require('express'),
     app = express();
 
+app.get('/', function (req, res) {
+  res.send("hello");
+});
+
 app.listen(3000, function () {
   console.log('server started on port 3000');
 });
